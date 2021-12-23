a37f55daddea4c7950e805aabeb96e7a07d86b42
joesi8302 <joesi@cpp.edu>
2021-12-23 11:37:27 -0800

    first commit

---
+++ src/test/java/com/revature/P2EarthBackend/P2EarthBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.P2EarthBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P2EarthBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
