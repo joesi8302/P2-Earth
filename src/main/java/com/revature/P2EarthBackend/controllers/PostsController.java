94ea5a7bb53c0db164d90fde90ffa8069bd719a4
joesi8302 <joesi@cpp.edu>
2021-12-27 14:59:37 -0800

    Added to PostsController and added relationship between tables

--- src/main/java/com/revature/P2EarthBackend/controllers/PostsController.java
+++ src/main/java/com/revature/P2EarthBackend/controllers/PostsController.java
@@ -1,10 +1,40 @@
 package com.revature.P2EarthBackend.controllers;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.revature.P2EarthBackend.models.Posts;
+import com.revature.P2EarthBackend.services.PostsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "posts")
 public class PostsController {
+
+    private PostsService postsService;
+
+    @Autowired
+    public PostsController(PostsService postsService){
+        this.postsService = postsService;
+    }
+
+    @GetMapping
+    public List<Posts> getAllPosts(){
+        return postsService.getAllPosts();
+    }
+
+    @GetMapping
+    public Posts getOnePost(@PathVariable Integer postId){
+        return postsService.getOnePost(postId);
+    }
+
+    @GetMapping List<Posts> getAllUserPosts(@PathVariable Integer userId){
+        return postsService.getAllUserPosts(userId);
+    }
+
+    @PutMapping Posts createPost(@RequestBody Posts post){
+        return postsService.createPost(post);
+    }
+
 }
