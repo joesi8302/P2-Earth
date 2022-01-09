932c6fba663bf8cc374b103011f3a4e3d969ce44
craazyjava <aziz0804@libero.it>
2022-01-09 01:10:58 -0500

    finished the get all paged posts by user id function

--- src/main/java/com/revature/P2EarthBackend/services/PagedPostsService.java
+++ src/main/java/com/revature/P2EarthBackend/services/PagedPostsService.java
@@ -2,8 +2,10 @@ package com.revature.P2EarthBackend.services;
 
 
 import com.revature.P2EarthBackend.models.Posts;
+import com.revature.P2EarthBackend.models.Users;
 import com.revature.P2EarthBackend.repository.PagedPostsDao;
 import com.revature.P2EarthBackend.repository.PostsDao;
+import com.revature.P2EarthBackend.repository.UsersDao;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -11,16 +13,20 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class PagedPostsService {
     private PagedPostsDao pagedPostsDao;
+    private UsersDao usersDao;
     Logger logger =Logger.getLogger(PostsService.class);
 
     @Autowired
-    public PagedPostsService(PagedPostsDao pagedPostsDao){
+    public PagedPostsService(PagedPostsDao pagedPostsDao,UsersDao usersDao){
         this.pagedPostsDao = pagedPostsDao;
+        this.usersDao=usersDao;
     }
 
     public List<Posts> getAllPagedPosts(Integer pageNo){
@@ -33,6 +39,32 @@ public class PagedPostsService {
 
         return postsList.toList();
     }
+
+//    public List<Posts> gettAllPafedPostsByUserId(Integer pageNo,Integer user_ID,Integer pageSize){
+//        Users user =usersDao.getById(user_ID);
+//
+//        Pageable paging=PageRequest.of(pageNo,pageSize);
+//
+//        Page<Posts> postList=this.pagedPostsDao.findByUserId(user,paging);
+//        return postList.toList();
+//    }
+public List<Posts> getAllPagedPostsByUserId(Integer pageNo,Integer user_id){
+
+    Pageable paging = PageRequest.of(pageNo, 20);
+
+    Page<Posts> postsList= this.pagedPostsDao.findAll(paging);
+
+
+    List<Posts> postListAllPosts=postsList.toList();
+    List<Posts> posts=new ArrayList<Posts>();
+    for(Integer i=0;i<postListAllPosts.size();i++){
+        if (postListAllPosts.get(i).getUser().getUser_id()==user_id){
+            posts.add(postListAllPosts.get(i));
+        }
+    }
+
+    return posts;
+}
 
 
 
