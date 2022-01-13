32782f034ad68d12e9dd4aac90a8f6e1bc431228
David-Bratkov <bratkovdavid09@gmail.com>
2022-01-12 18:46:53 -0800

    fixed the issues with the tests not being able to run all at once

--- src/test/java/com/revature/P2EarthBackend/repository/PostsDaoTestIT.java
+++ src/test/java/com/revature/P2EarthBackend/repository/PostsDaoTestIT.java
@@ -2,9 +2,12 @@ package com.revature.P2EarthBackend.repository;
 
 import com.revature.P2EarthBackend.models.Posts;
 import com.revature.P2EarthBackend.models.Users;
+import org.apache.catalina.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.sql.Timestamp;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -37,18 +40,19 @@ class PostsDaoTestIT {
     @Test
     void findAllByPostId() {
         //arrange
-        Users user1 = new Users(1,"archer01","123456","Sterling","Archer","","archer01@email.com");
+        Users user1 = new Users( "archer01","123456","Sterling","Archer","","archer01@email.com");
         usersDao.save(user1);
-        Posts post1 =new Posts("post_description1","post_img1",user1);
-        Posts post2 =new Posts("post_description2","post_img2",user1);
-        Posts postFromDB1 =postsDao.save(post1);
-        Posts postFromDB2 =postsDao.save(post2);
+        //Users users = usersDao.getById(0);
+        Posts post1 = new Posts(1L, new Timestamp(System.currentTimeMillis()), "post_description1","post_img1", user1, null);
+        Posts post2 = new Posts(2L, new Timestamp(System.currentTimeMillis()),"post_description2","post_img2", user1, null);
+        Posts postFromDB1 = postsDao.save(post1);
+        Posts postFromDB2 = postsDao.save(post2);
 
 
         //act
 
-        Posts actualResult=postsDao.findAllByPostId(post1.getPost_id());
-        Posts expectedResult=post1;
+        Posts actualResult = postsDao.findAllByPostId(post1.getPost_id());
+        Posts expectedResult = post1;
 
 
         //assert
